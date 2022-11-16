6a552e2fb34e12af0ea90a22de3ca068da15ec9a
viniocean <95971564+viniocean@users.noreply.github.com>
2022-11-16 10:36:07 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+jsonServer = require ('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
+console.log("TUDO BEM")
